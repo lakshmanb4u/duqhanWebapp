689be033948c45ce014c2693caa7a05f0abf951d
nandi-krisanu <nandi.krisanu@gmail.com>
2017-06-13 13:14:30 +0530

    small bug fixes

--- app/store/controllers/product-ctrl.js
+++ app/store/controllers/product-ctrl.js
@@ -1,203 +1,288 @@
 'use strict';
-angular.module('store')
-.controller('ProductCtrl', function ($log, $stateParams, $ionicSlideBoxDelegate, $state, $ionicActionSheet, $ionicModal, $scope, $rootScope, Store) {
+angular
+  .module('store')
+  .controller('ProductCtrl', function (
+    $log,
+    $stateParams,
+    $ionicSlideBoxDelegate,
+    $state,
+    $ionicActionSheet,
+    $ionicModal,
+    $scope,
+    $rootScope,
+    $timeout,
+    Store
+  ) {
+    /* Storing contextual this in a variable for easy access */
 
-  /* Storing contextual this in a variable for easy access */
+    var ctrl = this;
 
-  var ctrl = this;
+    $log.log(
+      'Hello from your Controller: ProductCtrl in module store:. This is your controller:',
+      ctrl
+    );
 
-  $log.log('Hello from your Controller: ProductCtrl in module store:. This is your controller:', ctrl);
-
-  /*=============================================
+    /*=============================================
   =            Get product details            =
   =============================================*/
 
-  /*----------  Initialize product object  ----------*/
+    /*----------  Initialize product object  ----------*/
 
-  ctrl.product = {};
+    ctrl.product = {};
 
-  /*----------  Get details of a product from backend  ----------*/
+    /*----------  Get details of a product from backend  ----------*/
 
-  ctrl.loadProductDetail = function (productId) {
-    var productParam = {'productId': productId};
-    Store.getProductDetail(productParam)
-    .then(function (response) {
-      $log.log(response.data);
-      ctrl.product = response.data;
-      $log.log(ctrl.images);
-      $log.log(ctrl.product.images);
-      var sizeArr = [];
-      if (ctrl.product.sizes) {
-        angular.forEach(ctrl.product.sizes, function (value) {
-          sizeArr.push(value.sizeText);
+    ctrl.loadProductDetail = function (productId) {
+      var productParam = { productId: productId };
+      Store.getProductDetail(productParam)
+        .then(function (response) {
+          $log.log(response.data);
+          ctrl.product = response.data;
+          $log.log(ctrl.images);
+          $log.log(ctrl.product.images);
+          var sizeArr = [];
+          if (ctrl.product.sizes) {
+            angular.forEach(ctrl.product.sizes, function (value) {
+              sizeArr.push(value.sizeText);
+            });
+          }
+          ctrl.product.sizeArr = sizeArr.toString();
+          var colorArr = [];
+          if (ctrl.product.sizes) {
+            angular.forEach(ctrl.product.colors, function (value) {
+              colorArr.push(value.colorText);
+            });
+          }
+          ctrl.product.colorArr = colorArr.toString();
+          $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
+          ctrl.productId = ctrl.product.productId;
+        })
+        .catch(function (response) {
+          $log.log(response);
         });
-      }
-      ctrl.product.sizeArr = sizeArr.toString();
-      var colorArr = [];
-      if (ctrl.product.sizes) {
-        angular.forEach(ctrl.product.colors, function (value) {
-          colorArr.push(value.colorText);
-        });
-      }
-      ctrl.product.colorArr = colorArr.toString();
-      $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
-      ctrl.productId = ctrl.product.productId;
-    })
-    .catch(function (response) {
-      $log.log(response);
-    });
-  };
+    };
 
-  /*----------  Storing url parameter (product id) in scope ----------*/
+    /*----------  Storing url parameter (product id) in scope ----------*/
 
-  ctrl.productId = $stateParams.productId;
+    ctrl.productId = $stateParams.productId;
 
-  /*----------  call the function at the time of initialization  ----------*/
+    /*----------  call the function at the time of initialization  ----------*/
 
-  ctrl.loadProductDetail($stateParams.productId);
+    ctrl.loadProductDetail($stateParams.productId);
 
-  /*=====  End of Get product details  ======*/
+    /*=====  End of Get product details  ======*/
 
+    /*==================================================
+  Section: Slider button to navigate throuh images
+  ==================================================*/
+    ctrl.slideIndex = 0;
+    ctrl.nextSlide = function () {
+      $ionicSlideBoxDelegate.next();
+    };
 
-  /*==========================================================================================
+    ctrl.previousSlide = function () {
+      $ionicSlideBoxDelegate.previous();
+    };
+
+    ctrl.slideChanged = function (index) {
+      ctrl.slideIndex = index;
+    };
+    /*==================================================
+  End: Slider button to navigate throuh images
+  ==================================================*/
+
+    /*==========================================================================================
   =            Helping functions to traverse through tabs in product details page            =
   ==========================================================================================*/
 
-  ctrl.gotoDescription = function () {
-    $log.log(ctrl.productId);
-    $state.go('store.product.description', {productId: ctrl.productId});
-  };
+    ctrl.gotoDescription = function () {
+      $log.log(ctrl.productId);
+      $state.go('store.product.description', { productId: ctrl.productId });
+    };
 
-  ctrl.gotoRelated = function () {
-    $state.go('store.product.related', {productId: ctrl.productId});
-  };
+    ctrl.gotoRelated = function () {
+      $state.go('store.product.related', { productId: ctrl.productId });
+    };
 
-  /*=====  End of Helping functions to traverse through tabs in product details page  ======*/
+    /*=====  End of Helping functions to traverse through tabs in product details page  ======*/
 
-  /*=================================================
+    /*=================================================
   =            Add a product to the cart            =
   =================================================*/
 
-  /*----------  Add to cart - get triggered when user press "Add to Bag" button from the product detail page ----------*/
+    /*----------  Add to cart - get triggered when user press "Add to Bag" button from the product detail page ----------*/
 
-  ctrl.addToBag = function (product) {
-    ctrl.productSelected = {};
-    ctrl.productSelected.productId = product.productId;
-    $log.log(product);
-    if (!product.sizes || !product.sizes.length > 0) {
-      return;
-    }
-    if (product.sizes[0].sizeId) {
-      ctrl.sizeModal();
-    } else if (product.sizes[0].sizeColorMap[0].colorId) {
-      ctrl.productSelected.size = ctrl.product.sizes[0];
-      ctrl.colorModal();
-    } else {
-      ctrl.productSelected.mapId = ctrl.product.sizes[0].sizeColorMap[0].mapId;
-      ctrl.addToBagPersist(ctrl.productSelected, ctrl.product);
-    }
-  };
-
-  /*----------  Send the product to the backend which user newly added to the cart  ----------*/
-
-  ctrl.addToBagPersist = function (productSelected, product) {
-    // delete productSelected.size;
-    $log.log(ctrl.productSelected);
-
-    Store.addToCart(productSelected)
-    .then(function (response) {
-      $log.log(response.data);
-      if (response.data.status === 'success') {
-        productSelected.response = 'Item Added to your Bag!';
-        ctrl.openModal(productSelected, product);
-        $rootScope.$emit('getCartTotalNumber');
-      } else if (response.data.status === 'Product already added') {
-        productSelected.response = 'Item is already in the Bag!';
-        ctrl.openModal(productSelected, product);
+    ctrl.addToBag = function (product) {
+      ctrl.productSelected = {};
+      ctrl.productSelected.productId = product.productId;
+      $log.log(product);
+      if (!product.sizes || !product.sizes.length > 0) {
+        return;
       }
-    })
-    .catch(function (response) {
-      $log.log(response);
-    });
-  };
+      if (product.sizes[0].sizeId) {
+        ctrl.sizeModal();
+      } else if (product.sizes[0].sizeColorMap[0].colorId) {
+        ctrl.productSelected.size = ctrl.product.sizes[0];
+        ctrl.colorModal();
+      } else {
+        ctrl.productSelected.mapId =
+          ctrl.product.sizes[0].sizeColorMap[0].mapId;
+        ctrl.addToBagPersist(ctrl.productSelected, ctrl.product);
+      }
+    };
 
-  /*----------  Ionic modal to show the response of addition of the product to the cart  ----------*/
+    /*----------  Send the product to the backend which user newly added to the cart  ----------*/
 
-  $ionicModal.fromTemplateUrl('store/templates/product/addedToBagModal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function (modal) {
-    ctrl.modal = modal;
-  });
+    ctrl.addToBagPersist = function (productSelected, product) {
+      // delete productSelected.size;
+      $log.log(ctrl.productSelected);
 
-  ctrl.openModal = function (productSelected, product) {
-    ctrl.productSelected = productSelected;
-    ctrl.product = product;
-    $log.log(ctrl.product);
-    $log.log(ctrl.productSelected);
-    ctrl.modal.show();
-  };
+      Store.addToCart(productSelected)
+        .then(function (response) {
+          $log.log(response.data);
+          if (response.data.status === 'success') {
+            productSelected.response = 'Item Added to your Bag!';
+            ctrl.openModal(productSelected, product);
+            $rootScope.$emit('getCartTotalNumber');
+          } else if (response.data.status === 'Product already added') {
+            productSelected.response = 'Item is already in the Bag!';
+            ctrl.openModal(productSelected, product);
+          }
+        })
+        .catch(function (response) {
+          $log.log(response);
+        });
+    };
 
-  ctrl.closeModal = function () {
-    ctrl.modal.hide();
-  };
+    /*----------  Ionic modal to show the response of addition of the product to the cart  ----------*/
 
-  /*----------  Open a Action sheet to select the size of the product  ----------*/
+    $ionicModal
+      .fromTemplateUrl('store/templates/product/addedToBagModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      })
+      .then(function (modal) {
+        ctrl.modal = modal;
+      });
 
-  ctrl.sizeModal = function () {
-    var buttons = [];
-    angular.forEach(ctrl.product.sizes, function (value) {
-      buttons.push({text: value.sizeText});
-    });
-    $ionicActionSheet.show({
-      buttons: buttons,
-      titleText: 'Select size',
-      cancelText: 'Cancel',
-      cancel: function () {
-        // add cancel code..
-      },
-      buttonClicked: function (index) {
-        // $log.log(index);
-        // $log.log(ctrl.product.sizes[index]);
-        ctrl.productSelected.size = ctrl.product.sizes[index];
-        if (ctrl.productSelected.size.sizeColorMap) {
-          ctrl.colorModal();
+    ctrl.openModal = function (productSelected, product) {
+      ctrl.productSelected = productSelected;
+      ctrl.product = product;
+      $log.log(ctrl.product);
+      $log.log(ctrl.productSelected);
+      ctrl.modal.show();
+    };
+
+    ctrl.closeModal = function () {
+      ctrl.modal.hide();
+    };
+
+    /*----------  Open a Action sheet to select the size of the product  ----------*/
+
+    ctrl.sizeModal = function () {
+      var buttons = [];
+      angular.forEach(ctrl.product.sizes, function (value) {
+        buttons.push({ text: value.sizeText });
+      });
+      $ionicActionSheet.show({
+        buttons: buttons,
+        titleText: 'Select size',
+        cancelText: 'Cancel',
+        cancel: function () {
+          // add cancel code..
+        },
+        buttonClicked: function (index) {
+          // $log.log(index);
+          // $log.log(ctrl.product.sizes[index]);
+          ctrl.productSelected.size = ctrl.product.sizes[index];
+          if (ctrl.productSelected.size.sizeColorMap) {
+            ctrl.colorModal();
+          }
+          return true;
         }
-        return true;
-      }
-    });
-  };
+      });
+    };
 
-  /*----------  Open a Action sheet to select the color of the product  ----------*/
+    /*----------  Open a Action sheet to select the color of the product  ----------*/
 
-  ctrl.colorModal = function () {
-    var buttons = [];
-    angular.forEach(ctrl.productSelected.size.sizeColorMap, function (value) {
-      buttons.push({text: value.colorText});
-    });
+    $ionicModal
+      .fromTemplateUrl('store/templates/product/colorModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      })
+      .then(function (modal) {
+        ctrl.colormodal = modal;
+      });
 
-    $ionicActionSheet.show({
-      buttons: buttons,
-      titleText: 'Select color',
-      cancelText: 'Cancel',
-      cancel: function () {
-        // add cancel code..
-      },
-      buttonClicked: function (index) {
-        // $log.log(index);
-        // $log.log(ctrl.productSelected.size.sizeColorMap[index]);
-        ctrl.productSelected.size.sizeColor = ctrl.productSelected.size.sizeColorMap[index];
-        ctrl.productSelected.mapId = ctrl.productSelected.size.sizeColor.mapId;
-        // $log.log(ctrl.productSelected.mapId);
+    ctrl.colorModal = function () {
+      ctrl.colorName = ctrl.productSelected.size.sizeColorMap;
+      ctrl.colormodal.show();
+
+      // var buttons = [];
+
+      // angular.forEach(ctrl.productSelected.size.sizeColorMap, function (value) {
+
+      //   buttons.push({ text: value.colorText });
+
+      // });
+
+      // $ionicActionSheet.show({
+
+      //   buttons: buttons,
+
+      //   titleText: 'Select color',
+
+      //   cancelText: 'Cancel',
+
+      //   cancel: function () {
+
+      //     // add cancel code..
+
+      //   },
+
+      //   buttonClicked: function (index) {
+
+      //     // $log.log(index);
+
+      //     // $log.log(ctrl.productSelected.size.sizeColorMap[index]);
+
+      //     ctrl.productSelected.size.sizeColor =
+
+      //       ctrl.productSelected.size.sizeColorMap[index];
+
+      //     ctrl.productSelected.mapId =
+
+      //       ctrl.productSelected.size.sizeColor.mapId;
+
+      //     // $log.log(ctrl.productSelected.mapId);
+
+      //     ctrl.addToBagPersist(ctrl.productSelected, ctrl.product);
+
+      //     return true;
+
+      //   }
+
+      // });
+    };
+
+    ctrl.colorSelected = function (index) {
+      ctrl.productSelected.size.sizeColor =
+        ctrl.productSelected.size.sizeColorMap[index];
+      ctrl.productSelected.mapId = ctrl.productSelected.size.sizeColor.mapId;
+      ctrl.closeColorModal();
+      $timeout(function () {
         ctrl.addToBagPersist(ctrl.productSelected, ctrl.product);
         return true;
-      }
+      }, 1000);
+    };
+
+    ctrl.closeColorModal = function () {
+      ctrl.colormodal.hide();
+    };
+
+    /*=====  End of Add a product to the cart  ======*/
+
+    $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
+      viewData.enableBack = true;
     });
-  };
-
-  /*=====  End of Add a product to the cart  ======*/
-
-  $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
-    viewData.enableBack = true;
   });
-});
